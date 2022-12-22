1dd2f8dd6a310e5aa1403aef48b46ff4f2c213ee
Andriy Paliychuk <wantstraddle@gmail.com>
2022-12-22 09:22:15 +0200

    add empty js file

---
+++ app.js
@@ -0,0 +1,96 @@
+// ARRAYS
+
+console.log("1. Create array using 4 different approaches");
+
+console.log("----------- FINISH ----------");
+
+console.log("2. Basic operations with array: ");
+console.log("- add el to the end of array");
+console.log("- add el to the beginning of array");
+console.log("- extract last");
+console.log("- extract first");
+
+const hobbies = ['Sports', 'Cooking'];
+
+console.log("----------- FINISH ----------");
+
+console.log("3. replacing \"Java\" & \"Lua\" with \"C\" & \"C++\" ");
+
+let languages = ["JavaScript", "Python", "Java", "Lua"];
+console.log(languages);
+
+console.log("3. add \"Java\" & \"Lua\" without deleting existing elements");
+
+console.log("3. remove 3 elements");
+
+console.log("----------- FINISH ----------");
+
+console.log("4. Create new array from numbers array. From numbers[3] to numbers[5]");
+
+let numbers1 = [2, 3, 5, 7, 11, 13, 17];
+console.log(numbers1);
+
+console.log("----------- FINISH ----------");
+
+console.log("5. Create new array by merging 2 arrays");
+
+let numbers2 = [2, 3, 5, 7, 11, 13, 17];
+console.log(numbers2);
+
+console.log("----------- FINISH ----------");
+
+console.log("6. Sort array from smaller to bigger then reverse it");
+
+const prices = [10.99, 5.99, 3.99, 6.59];
+
+console.log("----------- FINISH ----------");
+
+console.log("7. Join each element of the string");
+
+const message = ["JavaScript ", "is ", "fun."];
+
+console.log("-------------------")
+
+console.log("7. sum each element of the array");
+
+console.log("-------------------")
+
+const numbers4 = [1800, 50, 300, 20, 100];
+
+console.log("7. Subtract all numbers from first number");
+// since 1st element is called as accumulator rather than currentValue
+// 1800 - 50 - 300 - 20 - 100
+console.log("-------------------");
+
+console.log("7. Function that subtracts all array elements from given number");
+const expenses = [1800, 2000, 3000, 5000, 500];
+const salary = 15000;
+
+console.log("-------------------")
+
+console.log("7. Get rid duplicates");
+let ageGroup = [18, 21, 1, 1, 51, 18, 21, 5, 18, 7, 10];
+console.log("-------------------")
+
+console.log("7. Group by age");
+let people = [
+    { name: "John", age: 21 },
+    { name: "Oliver", age: 55 },
+    { name: "Michael", age: 55 },
+    { name: "Dwight", age: 19 },
+    { name: "Oscar", age: 21 },
+    { name: "Kevin", age: 55 },
+];
+console.log("----------- FINISH ----------");
+
+console.log("8. Create array from the string");
+const data = 'new york;10.99;2000';
+console.log("-------------------")
+
+console.log("8. Create string from array");
+const nameFragments = ['Max', 'Schwarz'];
+console.log("-------------------")
+
+console.log("8. Add elements from nameFragments to new array");
+
+console.log("----------- FINISH ----------");
