e8bdd9f7567e088b6cc7ba879844f8380ad448a8
Andriy Paliychuk <wantstraddle@gmail.com>
2022-12-27 17:34:37 +0200

    Callback, Promise, Async Await

---
+++ callback-promise-async-await.js
@@ -0,0 +1,36 @@
+//Callback Promise Async Await
+console.log("1. Create Callback Function and use it");
+
+console.log("----------- FINISH ----------");
+
+console.log("2. Create Callback Function with timeout and use it");
+
+console.log("----------- FINISH ----------");
+
+console.log("3. Create s Promise");
+
+console.log("----------- FINISH ----------");
+
+console.log("4. Chain Promise with then");
+
+console.log("----------- FINISH ----------");
+
+console.log("5. Chain Promise with catch");
+
+console.log("----------- FINISH ----------");
+
+console.log("6. Chain Promise with finally");
+
+console.log("----------- FINISH ----------");
+
+console.log("7. Callback hell example");
+
+console.log("----------- FINISH ----------");
+
+console.log("8. Create async function and chain it");
+
+console.log("----------- FINISH ----------");
+
+console.log("9. Create async function with try catch");
+
+console.log("----------- FINISH ----------");
